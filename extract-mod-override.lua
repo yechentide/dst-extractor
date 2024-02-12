bd32f22fb0c0353a450c1a66fa0b63cd542c0a34
yechentide <yechentide@gmail.com>
2024-02-12 10:37:40 +0900

    extract settings from worldgenoverride.lua/leveldataoverride.lua and modoverrides.lua

---
+++ extract-mod-override.lua
@@ -0,0 +1,42 @@
+if #arg == 0 then
+    print("Usage: lua extract-mod-override.lua ${path_to_shard_dir} <output_dir_path>")
+    os.exit(1)
+end
+
+-- ---------- ---------- ---------- ---------- ---------- ---------- --
+-- import util functions
+
+require("utils/shell")
+require("utils/file")
+require("utils/json")
+
+-- ---------- ---------- ---------- ---------- ---------- ---------- --
+-- prepare
+
+local shardDirPath = arg[1]
+local overrideFilePath = shardDirPath.."/modoverrides.lua"
+
+if not FileExists(overrideFilePath, false) then
+    print("Mod override file not found in "..shardDirPath)
+    os.exit(1)
+end
+
+local currentDirPath = ExecuteShellCommandReturnOutput("pwd")
+local outputDirPath = arg[2] or currentDirPath.."/output"
+if not FileExists(outputDirPath, true) then
+    local ok = MakeDir(outputDirPath, false)
+    if not ok then
+        print("Failed to create output directory in "..outputDirPath)
+        os.exit(1)
+    end
+end
+
+-- ---------- ---------- ---------- ---------- ---------- ---------- --
+
+local config = dofile(overrideFilePath)
+local jsonStr = ItemToJson(config, 0)
+
+local outputFilePath = outputDirPath.."/modoverrides.json"
+WriteToFile(outputFilePath, jsonStr)
+
+print("Completed! Output: "..outputFilePath)
---
+++ extract-worldgen-override.lua
@@ -0,0 +1,57 @@
+if #arg == 0 then
+    print("Usage: lua extract-worldgen-override.lua ${path_to_shard_dir} <output_dir_path>")
+    os.exit(1)
+end
+
+-- ---------- ---------- ---------- ---------- ---------- ---------- --
+-- import util functions
+
+require("utils/shell")
+require("utils/file")
+require("utils/json")
+
+-- ---------- ---------- ---------- ---------- ---------- ---------- --
+-- prepare
+
+local shardDirPath = arg[1]
+local overrideFilePath
+
+if not FileExists(shardDirPath, true) then
+    print("Shard directory not found in "..shardDirPath)
+    os.exit(1)
+end
+if FileExists(shardDirPath.."/worldgenoverride.lua", false) then
+    overrideFilePath = shardDirPath.."/worldgenoverride.lua"
+elseif FileExists(shardDirPath.."/leveldataoverride.lua", false) then
+    overrideFilePath = shardDirPath.."/leveldataoverride.lua"
+else
+    print("Worldgen override file not found in "..shardDirPath)
+    os.exit(1)
+end
+
+local currentDirPath = ExecuteShellCommandReturnOutput("pwd")
+local outputDirPath = arg[2] or currentDirPath.."/output"
+if not FileExists(outputDirPath, true) then
+    local ok = MakeDir(outputDirPath, false)
+    if not ok then
+        print("Failed to create output directory in "..outputDirPath)
+        os.exit(1)
+    end
+end
+
+-- ---------- ---------- ---------- ---------- ---------- ---------- --
+
+local config = dofile(overrideFilePath)
+local result = {
+    version = config.version,
+    id = config.id,
+    localtion = config.location,
+    playstyle = config.playstyle,
+    overrides = config.overrides,
+}
+local jsonStr = ItemToJson(result, 0)
+
+local outputFilePath = outputDirPath.."/worldgenoverride.json"
+WriteToFile(outputFilePath, jsonStr)
+
+print("Completed! Output: "..outputFilePath)
